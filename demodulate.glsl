0a12dc6090984f7919fa669836cde7343998ba3b
substack <substack@bits.coop>
2020-10-04 21:06:38 -1000

    interlacing

--- demodulate.glsl
+++ demodulate.glsl
@@ -1,28 +1,34 @@
 const float CARRIER_HZ = 3579545.0;
 const float PI = 3.1415927410125732;
-const float N_LINES = 262.0;
 float M = 1.0/CARRIER_HZ;
 const float T_LINE = 5.26e-5;
 
 #pragma glslify: yiq_to_rgb = require('./yiq_to_rgb.glsl')
 #pragma glslify: modulate = require('./modulate.glsl')
 
-vec4 read(float t, sampler2D signal) {
+vec4 read(float t, float n_lines, sampler2D signal) {
   return texture2D(signal, vec2(
     mod(t/T_LINE,1.0),
-    1.0-floor(t/T_LINE)/(N_LINES-1.0)
+    1.0-floor(t/T_LINE)/(n_lines-1.0)
   ));
 }
 
-vec3 demodulate(float t, sampler2D signal) {
+vec3 demodulate(float t, float n_lines, sampler2D signal) {
   float ti = t - mod(t,M) + M;
   float tq = t - mod(t,M) + M + M*0.25;
-  float ts = t - mod(t,M) + M + M*0.65;
-  float signal_i = read(ti, signal).x;
-  float signal_q = read(tq, signal).x;
-  float signal_s = read(ts, signal).x;
-  float y = min(signal_i,min(signal_q,signal_s)) * 0.5
-    + max(signal_i,max(signal_q,signal_s)) * 0.5;
+  float ta = t - mod(t,M) + M + M*0.65;
+  float tb = t - mod(t,M) + M + M*0.125;
+  float signal_i = read(ti, n_lines, signal).x;
+  float signal_q = read(tq, n_lines, signal).x;
+  float signal_a = read(ta, n_lines, signal).x;
+  float signal_b = read(tb, n_lines, signal).x;
+  float min_y = min(signal_i,signal_q);
+  min_y = min(min_y,signal_a);
+  min_y = min(min_y,signal_b);
+  float max_y = max(signal_i, signal_q);
+  max_y = max(max_y,signal_a);
+  max_y = max(max_y,signal_b);
+  float y = min_y*0.5 + max_y*0.5;
   return yiq_to_rgb(vec3(
     (y-7.5)/(100.0-7.5),
     (signal_i-y)/10.0,
--- example/ntsc.js
+++ example/ntsc.js
@@ -7,16 +7,27 @@ var regl = require('regl')({
   ]
 })
 
-var fbopts = {
-  color: regl.texture({
-    format: 'rgba',
-    type: 'float',
-    width: 720*4,
-    height: 262
-  }),
-  depth: true
-}
-var fbo = regl.framebuffer(fbopts)
+var fbopts = [
+  {
+    color: regl.texture({
+      format: 'rgba',
+      type: 'float',
+      width: 720*4-4,
+      height: 262
+    }),
+    depth: true
+  },
+  {
+    color: regl.texture({
+      format: 'rgba',
+      type: 'float',
+      width: 720*4-12,
+      height: 263
+    }),
+    depth: true
+  }
+]
+var fbo = [ regl.framebuffer(fbopts[0]), regl.framebuffer(fbopts[1]) ]
 
 var draw = {
   modulate: regl({
@@ -24,12 +35,11 @@ var draw = {
       precision highp float;
       #pragma glslify: modulate = require('../modulate.glsl')
       varying vec2 vpos;
-      uniform float time;
-      const float N_LINES = 262.0;
+      uniform float time, tick, n_lines;
       const float T_LINE = 5.26e-5;
       void main () {
         vec2 uv = vpos*vec2(1,-1)*0.5+0.5;
-        float t = uv.x*T_LINE + floor(uv.y*(N_LINES-1.0)+0.5)*T_LINE;
+        float t = uv.x*T_LINE + floor(uv.y*(n_lines-1.0)+0.5)*T_LINE;
         vec3 rgb = mix(
           vec3(0,1,1),
           vec3(1,0,1),
@@ -53,22 +63,27 @@ var draw = {
     elements: [0,1,2],
     framebuffer: regl.prop('framebuffer'),
     uniforms: {
-      time: regl.context('time')
+      time: regl.context('time'),
+      n_lines: regl.prop('n_lines')
     }
   }),
   demodulate: regl({
     frag: glsl`
       precision highp float;
       #pragma glslify: demodulate = require('../demodulate.glsl')
-      uniform sampler2D signal;
+      uniform sampler2D signal0, signal1;
       varying vec2 vpos;
-      uniform float time;
+      uniform float tick;
+      const float PI = ${Math.PI};
       const float N_LINES = 262.0;
       const float T_LINE = 5.26e-5;
       void main () {
         vec2 uv = vpos*vec2(1,-1)*0.5+0.5;
-        float t = uv.x*T_LINE + floor(uv.y*(N_LINES-1.0)+0.5)*T_LINE;
-        vec3 rgb = demodulate(t, signal);
+        float t0 = uv.x*T_LINE + floor(uv.y*(262.0-1.0)+0.5)*T_LINE;
+        float t1 = uv.x*T_LINE + floor(uv.y*(263.0-1.0)+0.5)*T_LINE;
+        vec3 rgb0 = demodulate(t0, 262.0, signal0) * mix(0.95,1.0,mod(tick,2.0));
+        vec3 rgb1 = demodulate(t1, 263.0, signal1) * mix(1.0,0.95,mod(tick,2.0));
+        vec3 rgb = mix(rgb0,rgb1,sin(uv.y*PI*2.0*262.5)*0.5+0.5);
         gl_FragColor = vec4(rgb,1);
       }
     `,
@@ -84,7 +99,9 @@ var draw = {
     attributes: { position: [-4,-4,-4,+4,+4,+0] },
     elements: [0,1,2],
     uniforms: {
-      signal: regl.prop('signal')
+      signal0: regl.prop('signal0'),
+      signal1: regl.prop('signal1'),
+      tick: regl.context('tick')
     }
   })
 }
@@ -95,9 +112,13 @@ frame()
 
 function frame() {
   regl.poll()
-  fbo(fbopts)
-  draw.modulate({ framebuffer: fbo })
+  fbo[tick%2](fbopts[tick%2])
+  draw.modulate({ framebuffer: fbo[tick%2], n_lines: tick%2 ? 263 : 262 })
   regl.clear({ color: [0,0,0,1], depth: true })
-  draw.demodulate({ signal: fbo })
+  draw.demodulate({
+    signal0: fbo[0],
+    signal1: fbo[1],
+  })
+  tick++
   window.requestAnimationFrame(frame)
 }
